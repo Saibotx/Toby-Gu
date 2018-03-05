54af74eb9fa3d311ae3cfcf6149be76e9376669e
tobias Gu <tobias.goo@gmail.com>
2018-03-05 13:53:44 -0800

    new config

--- code/static.config.js
+++ code/static.config.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { extractCritical } from "emotion-server";
+import { renderToString } from "react-dom/server";
 // Though this file is optional, there are SO MANY COOL THINGS you can do here.
 // Read the docs at https://github.com/nozzle/react-static/blob/master/README.md to learn more!
 export default {
@@ -15,6 +17,7 @@ export default {
       // const script1 = "window.child_process = require('child_process')";
       // const script2 =
       //   "window.lazySizesConfig = window.lazySizesConfig || {}; window.lazySizesConfig.loadMode = 1"
+      let extractedComp = extractCritical(renderToString(children));
       return (
         <Html lang="en-US">
           <Head>
@@ -36,8 +39,7 @@ export default {
             {renderMeta.styleTags}
             {/* <script dangerouslySetInnerHTML={{ __html: script1 }} /> */}
             {/* <script dangerouslySetInnerHTML={{ __html: script2 }} /> */}
-            {/* <script async defer src="https://apis.google.com/js/api.js" /> */}
-            {/* <script src="https://www.paypalobjects.com/api/checkout.js" /> */}
+            <style type="text/css">{extractedComp.css}</style>
           </Head>
           <Body style={{ margin: 0 }}>{children}</Body>
         </Html>
